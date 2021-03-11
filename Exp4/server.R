cdd04757656addcdcd0a44bd57060606a7d224da
Ak <akshaymali38@hotmail.com>
2021-03-11 15:46:44 +0530

    V1

---
+++ Exp4/server.R
@@ -0,0 +1,64 @@
+#
+# This is the server logic of a Shiny web application. You can run the
+# application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(ggplot2)
+# Define server logic required to draw a histogram
+shinyServer(function(input, output){
+    standup_frequency<-table(standup$listed_in)
+    cmovies<-as.data.frame(cmovies)
+   
+    abc <- reactive({
+        data[data$release.year==input$year,]
+          
+    })
+    
+    
+    output$q1 <- renderText({ "Helllooooo1" })
+    output$q2 <- renderText({ "Helllooooo2" })
+    output$q4 <- renderText({ "Helllooooo4" })
+    output$plot1<- renderPlot(width = 500,height = 500, ggplot(cmovies, aes(x="", y=prop, fill=Var1)) +
+                                  geom_bar(stat="identity", width=1, color="white") +
+                                  coord_polar("y", start=0) +
+                                  theme_void() + 
+                                  theme(legend.position="none") +
+                                  
+                                  geom_text(aes(y = ypos, label = Var1), color = "white", size=6) +
+                                  scale_fill_brewer(palette="Set1")
+    )  
+    
+    output$plot2 <- renderPlot(width = 500, height = 500,
+                               
+                               barplot(standup_frequency,ylab = "Genres",xlab = "No. of titles",horiz = TRUE,col = c("cyan"))
+                               
+    )
+    output$plot3 <- renderPlot(width = 500, height = 500,
+                               
+                               ggplot(abc(), aes(x=title, y=user.rating.score)) +
+                                   geom_segment( aes(x=title, xend=title, y=0, yend=user.rating.score), color="skyblue") +
+                                   geom_point( color="blue", size=4, alpha=0.6) +
+                                   theme_light() +
+                                   coord_flip() +
+                                   theme(
+                                       panel.grid.major.y = element_blank(),
+                                       panel.border = element_blank(),
+                                       axis.ticks.y = element_blank()
+                                   )
+                               
+    )
+    output$plot4 <- renderPlot(width = 1000, height = 600,
+                               
+                               
+                               ggplot(hrs1,aes(x=title, y=release.year, size=user.rating.score, color=title)) +
+                                   geom_point(alpha=0.5) +
+                                   scale_size(range = c(.1, 24), name="Rating")
+                               
+    )
+    
+})
---
+++ Exp4/ui.R
@@ -0,0 +1,85 @@
+#
+# This is the user-interface definition of a Shiny web application. You can
+# run the application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(shinydashboard)
+dashboardPage(
+    dashboardHeader(title = "Netflix dataset"),
+    dashboardSidebar(sidebarMenu(
+        menuItem("Children Movies", tabName = "Question1"),
+        menuItem("Standup Comedies", tabName = "Question2"),
+        menuItem("Most Watched Shows", tabName = "Question3"),
+        menuItem("Highest rated Show", tabName = "Question4")
+    )
+    ),
+    dashboardBody(
+        tabItems(
+            # First tab content
+            tabItem(tabName = "Question1",
+                    fluidRow(
+                        box(plotOutput("plot1", height = 250),height = 600),
+                        
+                        
+                    ),
+                    fluidRow(
+                        box(title = "Info",
+                            verbatimTextOutput("q1")
+                        )
+                    )
+            ),
+            
+            # Second tab content
+            tabItem(tabName = "Question2",
+                    # h2("Widgets tab content2")
+                    fluidRow(
+                        box(plotOutput("plot2", height = 250),height = 600),
+                        
+                        
+                    ),
+                    fluidRow(
+                        box(
+                            title = "Info",
+                            verbatimTextOutput("q2")
+                        )
+                    )
+            ),
+            # Third tab content
+            tabItem(tabName = "Question3",
+                    fluidRow(
+                        box(plotOutput("plot3", height = 250),height = 600),
+                        
+                        
+                    ),
+                    fluidRow(
+                        
+                        column(6,
+                               numericInput("year", "Enter any year from 1940 to 2017", value = 0, min = 0, max = 2017),
+                        )
+                        
+                    )
+            ),
+            # Fourth tab content
+            tabItem(tabName = "Question4",
+                    fluidRow(
+                        box(plotOutput("plot4", height = 600,width = 600),height = 700,width = 800),
+                        
+                        
+                    ),
+                    fluidRow(
+                        box(
+                            title = "Info",
+                            verbatimTextOutput("q4")
+                        )
+                    )
+            )
+            
+        )
+    )
+    
+)
